3d74f9f6b66618caa6ae9e59e4ec4a78e587788e
SKulikov2 <Kulikovsv.1987@mail.ru>
2023-03-27 23:28:33 +0700

    Update goods.js
    We got data, cars were created and rendered

--- js/goods.js
+++ js/goods.js
@@ -21,16 +21,20 @@ const getGoods = async function () {
 const createCard = function (objectCard) {
     const card = document.createElement('div');
 
+    console.log(objectCard);
+
     card.className = 'col-lg-3 col-sm-6';
 
     card.innerHTML = `
         <div class="goods-card">
-            <span class="label">New</span>
-            <img src="img/image-119.jpg" alt="image: Hoodie" class="goods-image">
-            <h3 class="goods-title">Embroidered Hoodie</h3>
-            <p class="goods-description">Yellow/Lilac/Fuchsia/Orange</p>
-            <button class="button goods-card-btn add-to-cart" data-id="007">
-                <span class="button-price">$89</span>
+        ${objectCard.label ? 
+            `<span class="label">${objectCard.label}</span>`:
+             ''}
+            <img src='db/${objectCard.img}' alt='${objectCard.name}' class="goods-image">
+            <h3 class="goods-title">${objectCard.name}</h3>
+            <p class="goods-description">${objectCard.description}</p>
+            <button class="button goods-card-btn add-to-cart" data-id='${objectCard.id}'>
+                <span class="button-price">$${objectCard.price}</span>
             </button>
         </div>
     `;
@@ -38,17 +42,19 @@ const createCard = function (objectCard) {
     return card;
 };
 
-// Функция для рендеринга карточек
+
+// Функция для рендеринга карточек на странице
 
 const renderCards = function (data) {
+
+// Эта переменная есть в DOM, с помощью действия ниже мы затираем содержимое longGoodsList 
+
     longGoodsList.textContent = '';
     const cards = data.map(createCard);
-    console.log(cards);
-
-
+    longGoodsList.append(...cards);
     document.body.classList.add('show-goods');
 };
-renderCards();
+getGoods().then(renderCards);
 
 
 
